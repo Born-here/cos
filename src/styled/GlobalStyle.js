d22266a32e7186f12f214860fa54ab0055714a11
loskang96 <gksthf9561@gmail.com>
2025-03-12 11:20:15 +0900

    globalstyle

--- src/styled/GlobalStyle.js
+++ src/styled/GlobalStyle.js
@@ -8,6 +8,38 @@ const breakpoints = {
     desktop: 1920,
 };
 
+// 색상 변수 설정
+export const colors = {
+    // 블랙 계열
+    black: '#231F20',
+    white: '#ffffff',
+
+    // 그레이 계열
+    gray100: '#231f20 ',
+    gray90: '#3e3e3f',
+    gray80: '#57585a',
+    gray70: '#6c6d70',
+    gray60: '#808285',
+    gray50: '#939598',
+    gray40: '#a8a9ad ',
+    gray30: '#bcbdc0',
+    gray20: '#d1d2d4 ',
+    gray10: '#e6e7e9',
+
+    // 프라이머리 계열 (파란색)
+    primary100: '#000000',
+    primary90: '#090e17',
+    primary80: '#181d2d',
+    primary70: '#282c44',
+    primary60: '#373b5a',
+    primary50: '#474e71',
+    primary40: '#5e6893',
+    primary30: '#868eae',
+    primary20: '#aeb4c9',
+    primary10: '#d7dae4',
+    primary0: '#ffffff',
+};
+
 // 폰트 변수 설정
 const fonts = {
     gowunDodum: "'GowunDodum', sans-serif", // 본문용 폰트 (고운돋움)
@@ -45,39 +77,39 @@ const GlobalStyle = createGlobalStyle`
     font-weight: normal;
     font-style: normal;
   }
-
+  
   /* 구글 Oswald 폰트 */
   @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;700&display=swap');
-
+  
   /* 기본 스타일 */
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-
+  
   /* 본문 폰트 적용 */
   body {
     font-family: ${fonts.gowunDodum}; /* 본문 폰트: 고운돋움 */
     color: #333;
     line-height: 1.6;
   }
-
+  
   /* 제목 및 버튼 폰트 적용 */
   h1, h2, h3, h4, h5, h6, button {
     font-family: ${fonts.oswald}; /* 강조 폰트: Oswald */
   }
-
+  
   /* 기본 스타일 */
   a {
     text-decoration: none;
     color: inherit;
   }
-
+  
   ul, ol {
     list-style: none;
   }
-
+  
   button {
     cursor: pointer;
   }
@@ -91,10 +123,11 @@ export const Container = styled.div`
 `;
 
 export const Inner = styled.div`
+    max-width: 1520px;
     width: 100%;
-    padding: 0 20px; /* 모바일 패딩 */
-
-    ${respondTo('tabletMore')} {
+    /* padding: 0 20px; */
+    
+    /* ${respondTo('tabletMore')} {
         padding: 0 30px; /* PC 패딩 */
-    }
+    } */
 `;
